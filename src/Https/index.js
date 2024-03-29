a464dac8f2e206927c12844d7d94da48ea545101
ZainUlBasit <zainulbasit486@gmail.com>
2024-03-29 18:05:32 +0500

    changes

--- src/Https/index.js
+++ src/Https/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BASE_URL_LOCAL } from "../utils/config";
+import { API_BASE_URL } from "../utils/config";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
