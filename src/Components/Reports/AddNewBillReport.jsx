2fa8d0af43232267a9d357d822fbff41fb1e1767
ZainUlBasit <zainulbasit486@gmail.com>
2024-05-19 13:32:05 +0500

    changes

--- src/Components/Reports/AddNewBillReport.jsx
+++ src/Components/Reports/AddNewBillReport.jsx
@@ -16,13 +16,13 @@ import BoldFont from "../../assets/fonts/Roboto-Bold.ttf";
 import { useEffect } from "react";
 
 // Register the fonts
-// Font.register({
-//   family: "CustomFont",
-//   fonts: [
-//     { src: RegularFont, fontWeight: "normal" },
-//     { src: BoldFont, fontWeight: "bold" },
-//   ],
-// });
+Font.register({
+  family: "CustomFont",
+  fonts: [
+    { src: RegularFont, fontWeight: "normal" },
+    { src: BoldFont, fontWeight: "bold" },
+  ],
+});
 
 // Create styles
 const styles = StyleSheet.create({
@@ -77,7 +77,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   cell1: {
-    // fontFamily: "CustomFont",
+    fontFamily: "CustomFont",
     fontWeight: "normal",
     width: "240px",
     fontSize: "12px",
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
     paddingLeft: "5px",
   },
   cell2: {
-    // fontFamily: "CustomFont",
+    fontFamily: "CustomFont",
     fontWeight: "normal",
     width: "100px",
     fontSize: "12px",
@@ -94,7 +94,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   cell3: {
-    // fontFamily: "CustomFont",
+    fontFamily: "CustomFont",
     fontWeight: "normal",
     width: "100px",
     fontSize: "12px",
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   cell4: {
-    // fontFamily: "CustomFont",
+    fontFamily: "CustomFont",
     fontWeight: "normal",
     width: "100px",
     fontSize: "12px",
@@ -123,7 +123,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   accountInfoTitle: {
-    // fontFamily: "CustomFont",
+    fontFamily: "CustomFont",
     fontWeight: "bold",
     fontSize: "13px",
     width: "120px",
@@ -132,7 +132,7 @@ const styles = StyleSheet.create({
   },
   accountInfo: {
     width: "100px",
-    // fontFamily: "CustomFont",
+    fontFamily: "CustomFont",
     fontWeight: "normal",
     fontSize: "13px",
     textAlign: "right",
@@ -164,7 +164,7 @@ function AddNewBillReport(props) {
         >
           <Text
             style={{
-              // fontFamily: "CustomFont",
+              fontFamily: "CustomFont",
               fontWeight: "bold",
               fontSize: "10px",
             }}
@@ -173,7 +173,7 @@ function AddNewBillReport(props) {
           </Text>
           <Text
             style={{
-              // fontFamily: "CustomFont",
+              fontFamily: "CustomFont",
               fontWeight: "bold",
               fontSize: "10px",
             }}
@@ -196,7 +196,7 @@ function AddNewBillReport(props) {
           {/* <View>
             <Text
               style={{
-                // fontFamily: "CustomFont",
+                fontFamily: "CustomFont",
                 fontWeight: "bold",
                 fontSize: 25,
                 marginBottom: 15,
@@ -207,7 +207,7 @@ function AddNewBillReport(props) {
             <View>
               <Text
                 style={{
-                  // fontFamily: "CustomFont",
+                  fontFamily: "CustomFont",
                   fontWeight: "bold",
                   fontSize: 15,
                 }}
@@ -216,7 +216,7 @@ function AddNewBillReport(props) {
               </Text>
               <Text
                 style={{
-                  // fontFamily: "CustomFont",
+                  fontFamily: "CustomFont",
                   fontWeight: "normal",
                   fontSize: 13,
                 }}
@@ -246,7 +246,7 @@ function AddNewBillReport(props) {
           <View>
             <Text
               style={{
-                // fontFamily: "CustomFont",
+                fontFamily: "CustomFont",
                 fontWeight: "bold",
                 fontSize: 13,
               }}
@@ -255,7 +255,7 @@ function AddNewBillReport(props) {
             </Text>
             <Text
               style={{
-                // fontFamily: "CustomFont",
+                fontFamily: "CustomFont",
                 fontWeight: "normal",
                 fontSize: 13,
               }}
@@ -264,7 +264,7 @@ function AddNewBillReport(props) {
             </Text>
             <Text
               style={{
-                // fontFamily: "CustomFont",
+                fontFamily: "CustomFont",
                 fontWeight: "normal",
                 fontSize: 13,
               }}
@@ -285,7 +285,7 @@ function AddNewBillReport(props) {
               <Text
                 style={{
                   textAlign: "right",
-                  // fontFamily: "CustomFont",
+                  fontFamily: "CustomFont",
                   fontWeight: "bold",
                   fontSize: 13,
                   width: "110px",
@@ -295,7 +295,7 @@ function AddNewBillReport(props) {
               </Text>
               <Text
                 style={{
-                  // fontFamily: "CustomFont",
+                  fontFamily: "CustomFont",
                   fontWeight: "normal",
                   fontSize: 13,
                   paddingLeft: 3,
@@ -315,7 +315,7 @@ function AddNewBillReport(props) {
               <Text
                 style={{
                   textAlign: "right",
-                  // fontFamily: "CustomFont",
+                  fontFamily: "CustomFont",
                   fontWeight: "bold",
                   fontSize: 13,
                   width: "110px",
@@ -325,7 +325,7 @@ function AddNewBillReport(props) {
               </Text>
               <Text
                 style={{
-                  // fontFamily: "CustomFont",
+                  fontFamily: "CustomFont",
                   fontWeight: "normal",
                   fontSize: 13,
                   paddingLeft: 3,
