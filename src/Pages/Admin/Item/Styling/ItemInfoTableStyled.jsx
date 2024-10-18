07993c24cf7493f97640b596ad1dde3d2c904426
ZainUlBasit <zainulbasit486@gmail.com>
2024-10-18 10:12:56 +0500

    changhes

--- src/Pages/Admin/Item/Styling/ItemInfoTableStyled.jsx
+++ src/Pages/Admin/Item/Styling/ItemInfoTableStyled.jsx
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const TableWrapper = styled.div`
   /* Tailwind="flex justify-center flex-col"> */
+  // margin: 0px 120px;
   margin-top: 1px;
   transition: all 0.5s ease-in-out;
   display: flex;
   justify-content: center;
   flex-direction: column;
-  // margin: 0px 120px;
   width: 100%;
   padding: ${(props) =>
     props.isAct ? "0px 20px 0px 140px" : "0px 20px 0px 20px"};
