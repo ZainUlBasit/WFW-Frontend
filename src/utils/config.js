af0278b7159d8b8816184fe24d79cf842fa2ecf4
ZainUlBasit <zainulbasit486@gmail.com>
2024-12-17 20:42:09 +0500

    changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://wfw-backend-app.up.railway.app/api";
+export const API_BASE_URL = "https://backend-wfw-production.up.railway.app/api";
 export const API_VERCEL = "https://wfw-backend-app.up.railway.app/api";
 export const API_BASE_URL_LOCAL = "http://localhost:8088/api";
