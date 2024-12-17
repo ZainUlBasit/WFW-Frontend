560ba0e242e1ef52ff14f8d55b66587fce3004f4
ZainUlBasit <zainulbasit486@gmail.com>
2024-12-17 20:29:03 +0500

    cchnages

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-export const API_BASE_URL = "https://wfw-backend-production.up.railway.app/api";
-export const API_VERCEL =
-  "https://wfw-backend-kzgl57zrb-zainulbasit.vercel.app/api";
+export const API_BASE_URL = "https://wfw-backend-app.up.railway.app/api";
+export const API_VERCEL = "https://wfw-backend-app.up.railway.app/api";
 export const API_BASE_URL_LOCAL = "http://localhost:8088/api";
